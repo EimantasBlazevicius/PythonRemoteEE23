9101059a4a2e5913fa74842a49e815b44208e124
EimantasBlazevicius <eima.blaz@gmail.com>
2024-02-18 13:40:53 +0200

    compelted register

--- Basics/Class 6/register.py
+++ Basics/Class 6/register.py
@@ -23,8 +23,20 @@ class Entry:
 
 print("Hello to the great annual Potato eating competition!")
 
-id_counter = 1
+id_counter = 0
 entries = []
+
+with open("entries.txt", "r+") as f:
+    headers = f.readline()
+    if headers == "":
+        f.write("id,name,age,city,potato_count\n")
+    else:
+        records = f.readlines()
+        for record in records:
+            line = record.strip().split(",")
+            entries.append(Entry(line[0], line[1], line[2], line[3], line[4]))
+            id_counter = int(line[0])
+last_id = id_counter
 
 while True:
     user_action = input("a - Add an entry\n"
@@ -33,19 +45,20 @@ while True:
                         ": ")
 
     if user_action.lower() == "a":
+        id_counter += 1
         entry = Entry(id_counter)
         entry.get_user_data()
         entries.append(entry)
-        id_counter += 1
     elif user_action.lower() == "v":
         for entry in entries:
-            print(entry.announcement())
+            entry.announcement()
     elif user_action.lower() == "x":
         break
 
-# with open("entries.txt", "x") as f:
-#     f.write("id,name,age,city,potato_count\n")
-
 with open("entries.txt", "a") as f:
     for entry in entries:
-        f.write(entry.form_entry())
+        # if entry.id > last id already present int he file:
+        if int(entry.id) > last_id:
+            f.write(entry.form_entry())
+
+print("Yeyy, saved")
