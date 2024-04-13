2d8b58967e5b7f5a7eba21d5da5ee051a686e1a8
EimantasBlazevicius <eima.blaz@gmail.com>
2024-04-13 10:48:17 +0300

    end of regex theory

--- PythonIntermediate/Class 3/regex_topic/regex_cheatsheet.py
+++ PythonIntermediate/Class 3/regex_topic/regex_cheatsheet.py
@@ -19,7 +19,7 @@ ac
 111.999.6699
 eima.blaz@gmail.com_'''
 
-patternText = re.compile(r'ac')  # Exact text
+patternText = re.compile(r'abcdefghijklmno')  # Exact text
 patternAnyText = re.compile(r'.')  # Any text
 patternDigits = re.compile(r'\d')  # All digits
 patternNotDigits = re.compile(r'\D')  # All not digits
@@ -33,7 +33,44 @@ patternNotWordBoundary = re.compile(r'\BHa')  # Individual occurrences of Ha
 patternStartString = re.compile(r'^abc')  # Start with string
 patterEndWithString = re.compile(r'.com_')  # Ends with String
 
-patternListOfOptions = re.compile(r'')
+patternListOfOptions = re.compile(r'[+-]37060482010')  # List of options
+patternNotListOfOption = re.compile(r'[^+-]37060482010')  # Not list of options
+patternGrouping = re.compile(r'\+(\d\d\d).(\d\d\d).(\d\d.\d\d)')  # Grouping
+patternGroupingWithOr = re.compile(r'\+(\d\d\d|\w\w\w).(\d\d\d).(\d\d.\d\d)')  # Grouping with OR
 
-result = re.findall(patterEndWithString, play_string)
-print(result)
+patternZeroOrMore = re.compile(r'\+370*')  # Zero or more
+patternOneOrMore = re.compile(r'\+370+')  # One or more
+patternOptionalCharacter = re.compile(r'ab?c')  # Optional Character
+patternExactNumber = re.compile(r'\+(\d{3}).(\d{3}).(\d{2}.\d{2})')
+patternRangeOfNumbers = re.compile(r'(\d{3}).(\d{3}).(\d{2,4})')
+
+# result = re.findall(patternExactNumber, play_string)
+# print(result)
+
+#  Find first occurrence of the pattern in the text
+resultSearch = patternText.search(play_string)
+print(resultSearch.group(0))
+
+#  Match the start of the text
+resultMatch = patternText.match(play_string)
+print(resultMatch)
+
+#  Find all occurrences
+resultFindAll = patternText.findall(play_string)
+print(resultFindAll)
+
+# Same as find all but returns an iterator
+resultFindAllIter = patternText.finditer(play_string)
+print(resultFindAllIter)
+
+# Split functionality
+resultSplit = patternWhiteSpace.split(play_string)
+print(resultSplit)
+
+# Substitute with another
+resultSub = patternWhiteSpace.sub('Potato', play_string)
+print(resultSub)
+
+# Substitute with another but shows how many substitutes done
+resultSubN = patternWhiteSpace.subn('Potato', play_string)
+print(resultSubN)
